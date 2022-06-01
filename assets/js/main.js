3d49f7f22323272cfc279e1da13b258b0d1f4e92
asdfjennifer <jennifergenger@gmailcom>
2022-06-02 00:32:19 +0200

    added keycode

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,3 +26,20 @@ enter.addEventListener("click", () => {
     ul.appendChild(listElement);
     listElement.textContent = input.value;
 });
+
+
+
+
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+    // If the user presses the "Enter" key on the keyboard
+    if (event.key === "Enter") {
+        // Cancel the default action, if needed
+        event.preventDefault();
+        // Trigger the button element with a click
+        let listElement = document.createElement("li");
+        ul.appendChild(listElement);
+        listElement.textContent = input.value;
+    }
+});
